6233ff0ee4cf282f0432de61c5d45bb916780449
zehraacarsarica <zehraacar559@gmail.com>
2025-12-28 21:53:26 +0300

    support CLI run for the rinpy package and improve init for font load

--- src/rinpy/__init__.py
+++ src/rinpy/__init__.py
@@ -7,12 +7,6 @@
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-import logging
-import os
-from importlib import resources
-
-import matplotlib.font_manager as fm
-import matplotlib.pyplot as plt
 
 __version__ = "0.0.1dev"
 __author__ = "Zehra Sarica <zehraacar559@gmail.com, sarica16@itu.edu.tr>"
@@ -22,23 +16,3 @@ import rinpy.logging_config
 from rinpy.rin_process import RINProcess
 
 __all__ = ["RINProcess"]
-
-
-def _load_fonts():
-    """Load packaged Times New Roman fonts dynamically."""
-    try:
-        with resources.path("rinpy.fonts", "times.ttf") as f:
-            font_dir = os.path.dirname(f)
-            for font_file in os.listdir(font_dir):
-                if font_file.endswith(".ttf"):
-                    fm.fontManager.addfont(os.path.join(font_dir, font_file))
-
-        plt.rcParams["font.family"] = "Times New Roman"
-        logging.info(f"Active font: {plt.rcParams['font.family'][0]}")
-    except Exception as e:
-        print(f"Warning: Could not load Times New Roman fonts ({e}). Using default font.")
-        plt.rcParams["font.family"] = "DejaVu Serif"
-        logging.warning(f"Could not load Times font ({e}). Using default {plt.rcParams['font.family'][0]}.")
-
-
-_load_fonts()
---
+++ src/rinpy/__main__.py
@@ -0,0 +1,4 @@
+from rinpy.rin_process import main
+
+if __name__ == "__main__":
+    main()
